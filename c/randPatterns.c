5683a614e7c73f4536786b3ee91f677057c5b17b
ayannw <ayanispro420@gmail.com>
2022-01-08 23:39:23 +0600

    update randPatterns.c

--- c/randPatterns.c
+++ c/randPatterns.c
@@ -9,15 +9,15 @@ static int drawRow(int n);
 int main() {
 
   seed = time(NULL);
-  
+
   printBoard(7);
 
   return 0;
 }
 
 static int printBoard(int s) {
-  
-  size = s;
+
+  size = s;    // height and width (in char) of output
   for(int i = 1;i < (size + 1);i++) {
     drawRow(i);
   }
@@ -30,7 +30,7 @@ static int drawRow(int n) {
   srand(seed);
   for(int i = n;i < (n + size);i++) {
     int r = rand() % (n + 1 - 0) + 0;
-    if(r % 5 != 0)
+    if(r % 3 != 0)
       printf("\033[0;36m██\033[0m");
     else
       printf("  ");
