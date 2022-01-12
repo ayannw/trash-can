7c5ba27bce485d165be8068dd5d357eb07b51210
ayannw <ayanispro420@gmail.com>
2022-01-12 23:03:39 +0600

    created password generator: passgen.c

---
+++ c/passgen.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+char *generate(int len);
+char pickRandom(char *str);
+
+int main() {
+
+  srand(time(NULL));
+
+  printf("%s\n", generate(8));
+
+  return 0;
+}
+
+char *generate(int n) {
+
+  char ALPHABETS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+  char numbers[] = "0123456789";
+  char symbols[] = "@#$~_&-+(){}[]^/*:;!?.%";
+
+  int randomizer = rand() % 4;
+  char *password = malloc(sizeof(char) * n);
+
+  for(int i = 0;i < n;i++) {
+    switch(randomizer) {
+      case 0:
+        password[i] = pickRandom(ALPHABETS);
+        randomizer = rand() % 4;
+        break;
+      
+      case 1:
+        password[i] = pickRandom(alphabets);
+        randomizer = rand() % 4;
+        break;
+
+      case 2:
+        password[i] = pickRandom(numbers);
+        randomizer = rand() % 4;
+        break;
+
+      case 3:
+        password[i] = pickRandom(symbols);
+        randomizer = rand() % 4;
+        break;
+    }
+  }
+  
+  return password;
+}
+
+char pickRandom(char *str) {
+  return str[rand() % strlen(str)];
+}
